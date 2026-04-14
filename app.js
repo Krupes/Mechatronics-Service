9a60aba539627b4f17d09848b2ace96175ca8772
Krups <smasher379zl@gmail.com>
2026-04-14 22:57:01 +0300

    Полное обновление проекта: новая версия

---
+++ app.js
@@ -0,0 +1,24 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('header-menu');
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    menu.classList.toggle('open');
+});
+menu.querySelectorAll('.menu-item').forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.remove('open');
+        menu.classList.remove('open');
+    });
+});
+const politicsButton = document.getElementsByClassName('politics_button')[0];
+const dialogPolitics = document.getElementsByClassName('dialog-politics')[0];
+const dialogPoliticsClose = document.getElementsByClassName('dialog-politics-close')[0];
+console.log(politicsButton);
+politicsButton.addEventListener('click', (e) => {
+    dialogPolitics .classList.toggle('politics-open');
+    document.body.classList.toggle('modal-open');
+})
+dialogPoliticsClose.addEventListener('click', (e) => {
+    dialogPolitics .classList.toggle('politics-open');
+    document.body.classList.toggle('modal-open');
+})
